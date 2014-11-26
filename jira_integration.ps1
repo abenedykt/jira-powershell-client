f3a0019d9a51e1048e6f3b14ac28dda42e4cd0aa
4rchie <arkadiusz@benedykt.net>
2014-11-26 08:50:23 +0100

    Update jira_integration.ps1
    added command Commit

--- jira_integration.ps1
+++ jira_integration.ps1
@@ -202,12 +202,50 @@ function Git-Commit {
 
 }
 
-
-function Hello(){
+function Commit {
+[CmdletBinding()]
     Param(
-        [string]$Name,
-        [string]$Surname
+        # Any other parameters can go here
+        [string]$Comment=""
     )
-    
-    Write-Output "name =  $($Name) surname = $($Surname)"
+ 
+    DynamicParam {
+            # Set the dynamic parameters' name
+            $ParameterName = 'Issue'
+            
+            # Create the dictionary 
+            $RuntimeParameterDictionary = New-Object System.Management.Automation.RuntimeDefinedParameterDictionary
+
+            # Create the collection of attributes
+            $AttributeCollection = New-Object System.Collections.ObjectModel.Collection[System.Attribute]
+            
+            # Create and set the parameters' attributes
+            $ParameterAttribute = New-Object System.Management.Automation.ParameterAttribute
+            $ParameterAttribute.Mandatory = $true
+            $ParameterAttribute.Position = 1
+
+            # Add the attributes to the attributes collection
+            $AttributeCollection.Add($ParameterAttribute)
+
+            # Generate and set the ValidateSet 
+            $arrSet = Jira-Issues-Keys
+            $ValidateSetAttribute = New-Object System.Management.Automation.ValidateSetAttribute($arrSet)
+
+            # Add the ValidateSet to the attributes collection
+            $AttributeCollection.Add($ValidateSetAttribute)
+
+            # Create and return the dynamic parameter
+            $RuntimeParameter = New-Object System.Management.Automation.RuntimeDefinedParameter($ParameterName, [string], $AttributeCollection)
+            $RuntimeParameterDictionary.Add($ParameterName, $RuntimeParameter)
+            return $RuntimeParameterDictionary
+    }
+
+    begin {
+        # Bind the parameter to a friendly variable
+        $SelectedIssueKey = $PsBoundParameters[$ParameterName] + ' ' + $Comment
+    }
+
+    process {
+        git commit -m $SelectedIssueKey
+    }
 }
